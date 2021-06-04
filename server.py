bc66633bf0db43bd37536729e29dec19fb7a1746
CentOS <centos@centos8.linuxvmimages.local>
2021-06-04 06:11:02 +0530

    Cambie el comando que se usa en usuarios_conectados.py 'w' -> 'who -H', tambien odifique el template verResultado

--- server.py
+++ server.py
@@ -10,19 +10,19 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/ver-usuarios')
-def ver_usuarios():
-    os.system('python3 ./usuarios_conectados.py > ./resultados/usuarios_conectados.txt')
-    texto = ''
+@app.route('/<program_name>')
+def dar_resultado(program_name):
+    resultado = []
+    titulo = str(program_name).replace("_", " ").capitalize() # Convertimos a string el nombre del programa, remplazamos el _ por <espacio> y ponemos mayuscula la primera letra
 
-    with open("./resultados/usuarios_conectados.txt", "r") as f:
-        texto = f.readline()
-        texto = f.read()
+    subprocess.run(["python3", f"{program_name}.py"]) # Corremos el programita en el cual guarda en un <program_name>.txt su resultado
 
-    with open("./resultados/usuarios_conectados.txt", "w") as f:
-        f.write(texto)
-    
-    return render_template("verResultado.html", texto = texto)
+    #Abrimos el archivo para leer que tiene el nombre del programa que se ejecuta anteriormente ej: <program_name>.txt
+    with open(f"./resultados/{program_name}.txt", "r") as file:
+        resultado = file.readlines() # Guardamos cada linea del txt en en una lista
+
+    # Renderizamos el template verResultado.html con el texto (resultado de la funcion) y su titulo (nombre del programa que se ejecuto)
+    return render_template("verResultado.html", texto = resultado, titulo = titulo)
 
 
 
--- usuarios_conectados.py
+++ usuarios_conectados.py
@@ -1,6 +1,23 @@
+#!/bin/bash python3
 import subprocess
 
+# Ejecuta el comando de linux "who -H" 
+# Para saber quienes son los usuarios conectados y desde donde.
+
 try:
-    subprocess.run("w")
+    byte_output = subprocess.check_output("who -H", shell=True) # El retorno es de tipo bytes y no str
+    output = byte_output.decode('utf-8') # decodeamos para que sea str
+    output = output.split('\n')
+    
+    output.pop(-1) # Eliminamos el ultimo valor de la lista que es un "'" que no se de donde sale
+    for out in output:
+        print(out)
+    
+    # Creamos o Reescribimos en un txt con el nombre de este programa .py
+    with open("./resultados/usuarios_conectados.txt", "w") as f:
+        for out in output:
+            f.write(out + "\n")
+        
+    
 except Exception:
     print("No se pudo mostrar los usuarios conectados")
