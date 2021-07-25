283674e1abbbf83d130f4555ee3756859386a657
root <root@localhost.localdomain>
2021-07-24 21:12:32 -0400

    una linea modifique

--- borrar.py
+++ borrar.py
@@ -1,6 +1,8 @@
-# from werkzeug.security import generate_password_hash, check_password_hash
-# hash_and_salted_password = generate_password_hash( "11223344", method='pbkdf2:sha256', salt_length=8)
-# print(hash_and_salted_password)
+from werkzeug.security import generate_password_hash, check_password_hash
+
+contrasenha = input("Ingrese una contrasenha para el usuario(admin) de hips:")
+hash_and_salted_password = generate_password_hash( contrasenha, method='pbkdf2:sha256', salt_length=8)
+print(hash_and_salted_password)
 
 
 
